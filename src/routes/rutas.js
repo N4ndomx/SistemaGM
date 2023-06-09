f4c43828fff55698ec276c05591cf4055ed5f63d
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-09 04:56:46 -0600

    clear folders

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -14,8 +14,8 @@ rutas.get('/datos/:matricula', (req, res) => { usuarioDatosControl.datosSoldado(
 rutas.get('/login/:matricula/:contra', (req, res) => {loginControl.validacionlogin(req, res)});
 
 // rutas.get("/",(req,res)=>{res.render("home")}) // Inicio de secion del sistema 
-// rutas.get("/registro-admin",(req,res)=>{res.render("Blog")})
-// rutas.get("/asig-mision",(req,res)=>{res.render("Blog")})
+rutas.get("/registro-admin",(req,res)=>{res.render("registrarAdmin")})
+rutas.get("/misiones",(req,res)=>{res.render("asignarMision")})
 rutas.get("/perfil",(req,res)=>{res.render("perfilCapitan")})
 // rutas.get("/estadisticas",(req,res)=>{res.render("Blog")})
 rutas.get("/home",(req,res)=>{res.render("home")})
