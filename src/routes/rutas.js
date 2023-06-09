5a018d06580431d582a2a4d11d102607867c7d86
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-09 05:11:10 -0600

    rutas finales

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -6,18 +6,17 @@ const loginControl = new loginViewController();
 const usuarioDatosControl = new usuarioDatosController();
 const rutas = Router();
 
-// Inicio de secion del sistema 
+// Inicio de sesion del sistema 
 rutas.get('/', (req, res) => { loginControl.index(req, res) });
 
 // Conexion con el BD
 rutas.get('/datos/:matricula', (req, res) => { usuarioDatosControl.datosSoldado(req, res) });
 rutas.get('/login/:matricula/:contra', (req, res) => {loginControl.validacionlogin(req, res)});
-
-// rutas.get("/",(req,res)=>{res.render("home")}) // Inicio de secion del sistema 
+// Rutas extra
 rutas.get("/registro-admin",(req,res)=>{res.render("registrarAdmin")})
 rutas.get("/misiones",(req,res)=>{res.render("asignarMision")})
 rutas.get("/perfil",(req,res)=>{res.render("perfilCapitan")})
-// rutas.get("/estadisticas",(req,res)=>{res.render("Blog")})
+ rutas.get("/estatus",(req,res)=>{res.render("estatus")})
 rutas.get("/home",(req,res)=>{res.render("home")})
 // // rutas.get("/blog",(req,res)=>{res.render("Blog")})
 
